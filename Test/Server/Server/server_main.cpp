dd19eaee2e6a4730fc501f6aedb8d3d9838df553
Tomson0119 <kjs980119@gmail.com>
2021-07-29 17:13:22 +0900

    Server Init

--- Test/Server/Server/server_main.cpp
+++ Test/Server/Server/server_main.cpp
@@ -1,7 +1,19 @@
 #include "../Net/common.h"
 #include <iostream>
 
+using namespace std;
+
+constexpr int Server_port = 5505;
+
 int main()
 {
-	std::cout << Print() << std::endl;
+	try {
+		Socket listenSck(SocketType::TCP);
+		listenSck.Bind(EndPoint("0.0.0.0", Server_port));
+
+	}
+	catch (Exception& ex)
+	{
+		cout << ex.what() << endl;
+	}
 }
