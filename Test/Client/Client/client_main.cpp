15ac199566b7e9ec70c3b6d8f5663d010e8e9d43
Tomson0119 <kjs980119@gmail.com>
2021-07-30 16:09:13 +0900

    클라이언트에서 접속을 시도하고 서버에 데이터를 보낸다.

--- Test/Client/Client/client_main.cpp
+++ Test/Client/Client/client_main.cpp
@@ -16,7 +16,34 @@ int main()
 		// localhost(127.0.0.1)에 연결을 요청한다.
 		clientSck.Connect(EndPoint("127.0.0.1", Server_port));
 
-		cout << "Connection Succeeded: " << endl;
+		cout << "Connection Succeeded\n";
+
+		cout << "Sending data...\n";
+		string data;
+		int recv_len = 0;
+		while (true)
+		{
+			data.clear();
+			getline(cin, data);
+
+			// 데이터를 문자열 끝과 함께 보낸다.
+			clientSck.Send(data.c_str(), data.size());
+
+			// 서버로부터 응답 데이터를 받는다.
+			// 데이터 끝은 의도치 않은 문자일 수 있으므로 비워버린다.
+			int recv_len = clientSck.Receive();
+			clientSck.mReceiveBuffer[recv_len] = 0;
+
+			if (clientSck.mReceiveBuffer == data)
+			{
+				cout << "Server received successfully\n";
+			}
+			else
+			{
+				
+			}
+		}
+		cout << "Closing connection...\n";
 	}
 	catch (Exception& ex)
 	{
