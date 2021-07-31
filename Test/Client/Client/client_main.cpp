f56df9607dfa32ca293a823520849beb5b8a2e9a
Tomson0119 <kjs980119@gmail.com>
2021-07-31 15:20:57 +0900

    파일에 저장된 서버 ip를 불러들여 연결을 시도한다.

--- Test/Client/Client/client_main.cpp
+++ Test/Client/Client/client_main.cpp
@@ -1,22 +1,36 @@
 #include "../Net/common.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 constexpr int Server_port = 5505;
 
+void BlockingTCPSocket();
+
 int main()
+{
+	BlockingTCPSocket();
+}
+
+void BlockingTCPSocket()
 {
 	try {
 		Socket clientSck(SocketType::TCP);
 		clientSck.Bind(EndPoint::Any);
 
-		cout << "Connecting to Server...\n";
-		
-		// localhost(127.0.0.1)에 연결을 요청한다.
-		clientSck.Connect(EndPoint("127.0.0.1", Server_port));
+		ifstream ipFile("server_ip.txt");
+		if (!ipFile.is_open())
+		{
+			throw Exception("Can't find file or directory");
+		}
+		string address;
+		ipFile >> address;
+		ipFile.close();
 
-		cout << "Connection Succeeded\n";
+		cout << "Connecting to Server...\n";
+
+		clientSck.Connect(EndPoint(address.c_str(), Server_port));
 
 		cout << "Sending data...\n";
 		string data;
@@ -34,13 +48,10 @@ int main()
 			int recv_len = clientSck.Receive();
 			clientSck.mReceiveBuffer[recv_len] = 0;
 
-			if (clientSck.mReceiveBuffer == data)
+			if (clientSck.mReceiveBuffer != data)
 			{
-				cout << "Server received successfully\n";
-			}
-			else
-			{
-				
+				cout << "Server did not received successfully\n";
+				break;
 			}
 		}
 		cout << "Closing connection...\n";
