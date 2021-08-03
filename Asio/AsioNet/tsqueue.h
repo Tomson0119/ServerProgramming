65f8a87305d95a881a112eea5904f41c5d30f54b
Tomson0119 <kjs980119@gmail.com>
2021-08-03 19:33:05 +0900

    tsqueue will wait if queue is empty

--- Asio/AsioNet/tsqueue.h
+++ Asio/AsioNet/tsqueue.h
@@ -29,15 +29,21 @@ namespace net
 		// Adds an item to back of Queue
 		void push_back(const T& item)
 		{
-			std::scoped_lock lock(mtxQueue);
+			std::scoped_lock sl(mtxQueue);
 			deq.emplace_back(std::move(item));
+
+			std::unique_lock<std::mutex> ul(mtxBlocking);
+			cvBlocking.notify_one();
 		}
 
 		// Adds an item to front of Queue
 		void push_front(const T& item)
 		{
-			std::scoped_lock lock(mtxQueue);
+			std::scoped_lock sl(mtxQueue);
 			deq.emplace_front(std::move(item));
+
+			std::unique_lock<std::mutex> ul(mtxBlocking);
+			cvBlocking.notify_one();
 		}
 
 		// Removes and return item from front of Queue
@@ -76,8 +82,23 @@ namespace net
 			deq.clear();
 		}
 
+		void wait()
+		{
+			// Even if cv work up erronously,
+			// it will go back to sleep, because deq is empty.
+			while (empty())
+			{
+				// Wait until thread signals cv.
+				std::unique_lock<std::mutex> lock(mtxBlocking);
+				cvBlocking.wait(lock);
+			}
+		}
+
 	protected:
 		std::mutex mtxQueue;
 		std::deque<T> deq;
+
+		std::condition_variable cvBlocking;
+		std::mutex mtxBlocking;
 	};
 }
