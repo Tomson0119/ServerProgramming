e571500ae3d88f72f2c4cae608903beeb6cf439c
Tomson0119 <kjs980119@gmail.com>
2021-09-26 12:11:25 +0900

    initial - network 헤더 및 기본 파일

---
+++ GameServer/Network/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <exception>
+
+#include "NetException.h"
