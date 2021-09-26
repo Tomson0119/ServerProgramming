95522614d5f9c76e48d5a73da1b6b84a7a6ceb08
Tomson0119 <kjs980119@gmail.com>
2021-09-26 12:18:50 +0900

    refactor - const char* 형태로 오류 반환하기
    exception의 what을 오버라이드하기 위해 const char*로 변환하여 리턴한다.

--- GameServer/ClientApp/dxException.cpp
+++ GameServer/ClientApp/dxException.cpp
@@ -1,15 +1,23 @@
 #include "stdafx.h"
 #include "dxException.h"
+#include <cstdlib>
 
 DxException::DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& fileName, int lineNumber)
 	: mError(hr), mFuncName(functionName), mFileName(fileName), mLineNumber(lineNumber)
 {
 }
 
-std::wstring DxException::ToString() const
+DxException::~DxException()
+{
+}
+
+const char* DxException::what() const
 {
 	_com_error error(mError);
-	std::wstring msg = error.ErrorMessage();
-	return mFuncName + L" failed in " + mFileName + L";  line "
-		+ std::to_wstring(mLineNumber) + L";  Error : " + msg;
+	std::wstring result = mFuncName + L" failed in " + mFileName + L";  line "
+		+ std::to_wstring(mLineNumber) + L";  Error : " + error.ErrorMessage();
+	char msg[1024];
+	int len = WideCharToMultiByte(CP_ACP, 0, result.c_str(), -1, NULL, 0, NULL, NULL);
+	WideCharToMultiByte(CP_ACP, 0, result.c_str(), -1, msg, len, NULL, NULL);
+	return msg;
 }
--- GameServer/ClientApp/dxException.h
+++ GameServer/ClientApp/dxException.h
@@ -1,13 +1,14 @@
 #pragma once
 
-class DxException
+class DxException : public std::exception
 {
 public:
 	DxException() = default;
 	DxException(HRESULT hr, const std::wstring& functionName,
 		const std::wstring& fileName, int lineNumber);
+	virtual ~DxException();
 
-	std::wstring ToString() const;
+	virtual const char* what() const;
 
 	HRESULT mError = S_OK;
 	std::wstring mFuncName;
