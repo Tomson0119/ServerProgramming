5c2433ca524e2556603c0f4d6cb8ce52463ad8d4
Tomson0119 <kjs980119@gmail.com>
2021-10-11 18:42:33 +0900

    refactor - 클라이언트 소켓 작업
    클라이언트 소켓은 필요한 메세지를 서버에 보낸다.
    정의되어 있는 오버랩 확장 구조체를 채워넣어서 보낸다.
    콜백 함수를 여기서 정의하여 필요한 작업을 할 수 있도록 한다.

--- GameServer/ClientApp/clientSocket.cpp
+++ GameServer/ClientApp/clientSocket.cpp
@@ -1,16 +1,85 @@
 #include "stdafx.h"
 #include "clientSocket.h"
 
-ClientSocket::ClientSocket(Protocol type)
-	: Socket(type)
+std::vector<PlayerCoord> ClientSocket::PlayerCoords;
+
+void Socket::SendRoutine(DWORD err, DWORD bytes, LPWSAOVERLAPPED overlapped, DWORD flag)
 {
+}
+
+void Socket::RecvRoutine(DWORD err, DWORD bytes, LPWSAOVERLAPPED overlapped, DWORD flag)
+{
+	WSAOVERLAPPEDEX* recvOverEx = reinterpret_cast<WSAOVERLAPPEDEX*>(overlapped);
+	ClientSocket* caller = reinterpret_cast<ClientSocket*>(recvOverEx->Caller);
+	caller->HandleMessage(caller->mReceiveBuffer);
+
+	caller->RecvMsg();
+}
+
+ClientSocket::ClientSocket(Protocol type)
+	: Socket(type), ID(-1)
+{
+	int tcp_opt = 1;
+	setsockopt(mSocket, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<char*>(&tcp_opt), sizeof(int));
 }
 
 ClientSocket::~ClientSocket()
 {
 }
 
+void ClientSocket::SendMsg(Message& msg)
+{
+	ZeroMemory(&mSendOverlapped, sizeof(mSendOverlapped));
+	mSendOverlapped.WSABuffer.buf = reinterpret_cast<char*>(&msg);
+	mSendOverlapped.WSABuffer.len = (ULONG)msg.GetPacketSize();
+	mSendOverlapped.Caller = this;
+
+	Send(mSendOverlapped);
+}
+
+void ClientSocket::RecvMsg()
+{
+	ZeroMemory(&mRecvOverlapped, sizeof(mRecvOverlapped));
+	mRecvOverlapped.WSABuffer.buf = reinterpret_cast<char*>(&mReceiveBuffer);
+	mRecvOverlapped.WSABuffer.len = MaxRecvSize;
+	mRecvOverlapped.Caller = this;
+
+	Recv(mRecvOverlapped);
+}
+
 void ClientSocket::Disconnect()
 {
-	Send(Message(MsgType::MSG_DISCONNECT));
+	
+}
+
+void ClientSocket::HandleMessage(Message& msg)
+{
+	switch (msg.Header.msg_type)
+	{
+	case MsgType::MSG_ACCEPT:
+	{
+		ID = msg.Body[0];
+		for (int i = 0; i < msg.Header.size - 1; i += 2)
+		{
+			uint8_t x = msg.Body[i + 1];
+			uint8_t y = msg.Body[i + 2];
+			PlayerCoords.push_back({ x,y });
+		}
+		break;
+	}
+	case MsgType::MSG_JOIN:
+	{
+		uint8_t x = msg.Body[0];
+		uint8_t y = msg.Body[1];
+		PlayerCoords.push_back({ x,y });
+		break;
+	}
+	case MsgType::MSG_MOVE:
+	{
+		int id = (int)msg.Body[0];
+		uint8_t x = msg.Body[1];
+		uint8_t y = msg.Body[2];
+		PlayerCoords[id] = { x,y };
+		break;
+	}}
 }
--- GameServer/ClientApp/clientSocket.h
+++ GameServer/ClientApp/clientSocket.h
@@ -6,5 +6,18 @@ public:
 	ClientSocket(Protocol type);
 	virtual ~ClientSocket();
 
+	void SendMsg(Message& msg);
+	void RecvMsg();
+
 	void Disconnect();
+
+	void HandleMessage(Message& msg);
+
+public:
+	static std::vector<PlayerCoord> PlayerCoords;
+	int ID;
+
+private:
+	WSAOVERLAPPEDEX mSendOverlapped;
+	WSAOVERLAPPEDEX mRecvOverlapped;
 };
