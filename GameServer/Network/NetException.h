5e584f243e4d83d796b9e43b29ab982cd97edaea
Tomson0119 <kjs980119@gmail.com>
2021-12-01 23:57:07 +0900

    msic - minor changes

--- GameServer/Network/NetException.h
+++ GameServer/Network/NetException.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <stdexcept>
+
 class NetException : public std::exception
 {
 public:
 	NetException(const std::string& info);
 	virtual ~NetException();
 
-	virtual const char* what() const;
+	virtual const char* what() const override;
 
 private:
 	std::string mErrorString;
