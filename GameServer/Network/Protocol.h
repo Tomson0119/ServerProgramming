9aafb726236e31250ae7a257768475bd3bc6a135
Tomson0119 <kjs980119@gmail.com>
2021-12-04 23:03:02 +0900

    feat - 새로운 프로토콜

--- GameServer/Network/Protocol.h
+++ GameServer/Network/Protocol.h
@@ -1,89 +1,100 @@
 #pragma once
 
-const char* const SERVER_IP = "127.0.0.1";
 const short SERVER_PORT = 4000;
-
-const int  MaxBufferSize = 1024;
-const int  RANGE = 3;
 
 const int  WORLD_HEIGHT = 2000;
 const int  WORLD_WIDTH = 2000;
-
-const int  SECTOR_WIDTH_COUNT = 10;
-const int  SECTOR_HEIGHT_COUNT = 10;
-
-const int  SECTOR_WIDTH = WORLD_WIDTH / SECTOR_WIDTH_COUNT;
-const int  SECTOR_HEIGHT = WORLD_HEIGHT / SECTOR_HEIGHT_COUNT;
-
 const int  MAX_NAME_SIZE = 20;
 const int  MAX_CHAT_SIZE = 100;
 const int  MAX_USER = 10000;
-const int  MAX_NPC = 60000;
-
+const int  MAX_NPC = 200000;
 constexpr int NPC_ID_START = MAX_USER;
 constexpr int NPC_ID_END = MAX_USER + MAX_NPC - 1;
 
+constexpr int SECTOR_WIDTH = WORLD_WIDTH / 10;
+constexpr int SECTOR_HEIGHT = WORLD_HEIGHT / 10;
+
 const char CS_PACKET_LOGIN = 1;
 const char CS_PACKET_MOVE = 2;
+const char CS_PACKET_ATTACK = 3;
+const char CS_PACKET_CHAT = 4;
+const char CS_PACKET_TELEPORT = 5;
+
 
 const char SC_PACKET_LOGIN_OK = 1;
 const char SC_PACKET_MOVE = 2;
 const char SC_PACKET_PUT_OBJECT = 3;
 const char SC_PACKET_REMOVE_OBJECT = 4;
 const char SC_PACKET_CHAT = 5;
+const char SC_PACKET_LOGIN_FAIL = 6;
+const char SC_PACKET_STATUS_CHANGE = 7;
 
-#define LOGIN_WITH_ID
+const int MaxBufferSize = 1024;
+const int RANGE = 7;
 
 #pragma pack (push, 1)
-struct cs_packet_login 
-{
+struct cs_packet_login {
 	unsigned char size;
-	char	      type;
-	char		  name[MAX_NAME_SIZE];
+	char	type;
+	char	name[MAX_NAME_SIZE];
 };
 
-struct cs_packet_move 
-{
+struct cs_packet_move {
 	unsigned char size;
-	char          type;
-	char          direction;  // 0 : up,  1: down, 2:left, 3:right
-	int			  move_time;
+	char	type;
+	char	direction;			// 0 : up,  1: down, 2:left, 3:right
+	int		move_time;
 };
 
-struct sc_packet_login_ok 
-{
+struct cs_packet_attack {
 	unsigned char size;
-	char		  type;
-	bool		  success;
-	int			  id;
-	short		  x, y;
-	char		  name[MAX_NAME_SIZE];
+	char	type;
 };
 
-struct sc_packet_move 
-{
+struct cs_packet_chat {
 	unsigned char size;
-	char		  type;
-	int			  id;
-	short		  x, y;
-	int			  move_time;
+	char	type;
+	char	message[MAX_CHAT_SIZE];
 };
 
-struct sc_packet_put_object
-{
+struct cs_packet_teleport { 
+	// 서버에서 장애물이 없는 랜덤 좌표로 텔레포트 시킨다.
+	// 더미 클라이언트에서 동접 테스트용으로 사용.
 	unsigned char size;
-	char		  type;
-	int			  id;
-	short		  x, y;
-	char		  object_type;
-	char		  name[MAX_NAME_SIZE];
+	char	type;
 };
 
-struct sc_packet_remove_object
-{
+struct sc_packet_login_ok {
 	unsigned char size;
-	char		  type;
-	int			  id;
+	char type;
+	int		id;
+	short	x, y;
+	short	level;
+	short	hp, maxhp;
+	int		exp;
+};
+
+struct sc_packet_move {
+	unsigned char size;
+	char type;
+	int		id;
+	short  x, y;
+	int		move_time;
+};
+
+struct sc_packet_put_object {
+	unsigned char size;
+	char type;
+	int id;
+	short x, y;
+	char object_type;
+	char	name[MAX_NAME_SIZE];
+};
+
+struct sc_packet_remove_object {
+	unsigned char size;
+	char type;
+	int id;
 };
 
 struct sc_packet_chat {
@@ -92,4 +103,19 @@ struct sc_packet_chat {
 	int id;
 	char message[MAX_CHAT_SIZE];
 };
+
+struct sc_packet_login_fail {
+	unsigned char size;
+	char type;
+	char reason;		// 0: 중복 ID,  1:사용자 Full
+};
+
+struct sc_packet_status_change {
+	unsigned char size;
+	char type;
+	short	level;
+	short	hp, maxhp;
+	int		exp;
+};
+
 #pragma pack(pop)
