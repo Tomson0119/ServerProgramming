22f840c31c53cd9f96c40b5bdd64f9e5a935423c
Tomson0119 <kjs980119@gmail.com>
2021-12-04 23:03:51 +0900

    feat - 채팅 윈도우
    채팅 윈도우를 구현하여 다른 사람과 채팅을 할 수 있다.

---
+++ GameServer/D2DApp/ChatWindow.cpp
@@ -0,0 +1,130 @@
+#include "stdafx.h"
+#include "ChatWindow.h"
+#include "NetClient.h"
+
+
+#define IDC_INPUT_TEXT 0
+#define IDC_EDIT_LIST 1
+
+HWND ChatWindow::gEditList;
+HWND ChatWindow::gInputText;
+WNDPROC ChatWindow::gOldInputProc;
+NetClient* ChatWindow::gNetPtr;
+
+
+ChatWindow::ChatWindow()
+	: mWinCaption{ L"Message Log" },
+	  mWidth(400), mHeight(500), mHwnd(0)
+{
+}
+
+ChatWindow::~ChatWindow()
+{
+}
+
+bool ChatWindow::InitWindow(const std::wstring& classname)
+{
+	if (!RegisterWindowClass(classname)) return false;
+	if (!(mHwnd = CreateWindowHandle(classname))) return false;
+	return true;
+}
+
+void ChatWindow::SetNetworkPtr(NetClient* net)
+{
+	gNetPtr = net;
+}
+
+void ChatWindow::SetPosition(RECT& rect)
+{
+	SetWindowPos(mHwnd, NULL, rect.left, rect.top, 0, 0, SWP_NOZORDER | SWP_NOSIZE | SWP_NOACTIVATE);
+}
+
+bool ChatWindow::RegisterWindowClass(const std::wstring& classname)
+{
+	WNDCLASSEX wndClass{};
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WinProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = GetModuleHandle(NULL);
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = classname.c_str();
+	wndClass.hIconSm = LoadIcon(wndClass.hInstance, IDI_APPLICATION);
+	return (bool)RegisterClassEx(&wndClass);
+}
+
+HWND ChatWindow::CreateWindowHandle(const std::wstring& classname)
+{
+	RECT rect = { 0, 0, mWidth, mHeight};
+
+	return CreateWindow(classname.c_str(), mWinCaption.c_str(),
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		rect.right - rect.left, rect.bottom - rect.top, NULL, NULL,
+		GetModuleHandle(NULL), this);
+}
+
+void ChatWindow::AppendMessage(char id, const char* msg)
+{
+	int idx = GetWindowTextLength(gEditList);
+	std::string chat = "Player " + std::to_string(id) 
+		+ ": " + msg + "\r\n";
+	SendMessage(gEditList, EM_SETSEL, (WPARAM)idx, (LPARAM)idx);
+	SendMessageA(gEditList, EM_REPLACESEL, 0, (LPARAM)chat.c_str());
+}
+
+void ChatWindow::Run()
+{
+	::ShowWindow(mHwnd, SW_SHOW);
+	::UpdateWindow(mHwnd);
+}
+
+LRESULT ChatWindow::WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CREATE:
+		gEditList = CreateWindow(L"EDIT", L"", 
+			WS_BORDER | WS_CHILD | WS_VISIBLE | EM_SETREADONLY
+			| WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL,
+			10, 10, 360, 400, hwnd, (HMENU)IDC_EDIT_LIST, 0, 0);
+		EnableWindow(gEditList, FALSE);
+
+		gInputText = CreateWindow(L"EDIT", L"",
+			WS_BORDER | WS_CHILD | WS_VISIBLE
+			| ES_LEFT | ES_AUTOVSCROLL,
+			10, 420, 360, 30, hwnd, (HMENU)IDC_INPUT_TEXT, 0, 0);
+		gOldInputProc = (WNDPROC)SetWindowLongPtr(gInputText, GWLP_WNDPROC, (LONG_PTR)InputProc);
+		break;
+
+	case WM_DESTROY:		
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+LRESULT ChatWindow::InputProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if(msg == WM_CHAR && wParam == VK_RETURN)
+	{
+		int size = GetWindowTextLengthW(hwnd) + 1;
+		if (size <= 1) return 0;
+
+		std::wstring message;
+		message.resize(size);
+		int len = GetWindowTextW(hwnd, (LPWSTR)message.c_str(), size);
+			
+		if (len != 0)
+		{
+			std::string str = WStringToString(message);
+			gNetPtr->SendChatPacket(str.c_str());
+			SetWindowText(hwnd, L"");				
+		}
+		return 0;
+	}
+	return CallWindowProc(gOldInputProc, hwnd, msg, wParam, lParam);
+}
+
---
+++ GameServer/D2DApp/ChatWindow.h
@@ -0,0 +1,39 @@
+#pragma once
+
+class NetClient;
+
+class ChatWindow
+{
+public:
+	ChatWindow();
+	virtual ~ChatWindow();
+
+	bool InitWindow(const std::wstring& classname);
+	void SetNetworkPtr(NetClient* net);
+	void SetPosition(RECT& rect);
+	void Run();
+
+	static void AppendMessage(char id, const char* msg);
+
+private:
+	bool RegisterWindowClass(const std::wstring& classname);
+	HWND CreateWindowHandle(const std::wstring& classname);
+
+public:
+	static LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK InputProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+private:
+	std::wstring mWinCaption;
+
+	HWND mHwnd;
+	static HWND gEditList;
+	static HWND gInputText;
+
+	static WNDPROC gOldInputProc;
+
+	int mWidth;
+	int mHeight;
+
+	static NetClient* gNetPtr;
+};
