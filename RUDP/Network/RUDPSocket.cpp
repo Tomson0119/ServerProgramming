fa5db2c8a17c8acc0288454ed06bfad43529130f
Tomson0119 <kjs980119@gmail.com>
2022-07-18 21:18:40 +0900

    send main queue를 전용 thread에서 관리

--- RUDP/Network/RUDPSocket.cpp
+++ RUDP/Network/RUDPSocket.cpp
@@ -2,26 +2,35 @@
 #include <iostream>
 #include <exception>
 
+using namespace std::chrono_literals;
+
 RUDPSocket::RUDPSocket(const SckType& type)
-	: hostEp{}
+	: hostEp{}, loop{ true }
 {
-	if (type == SckType::TCP)
-	{
-		sckHandle = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	}
-	else
-	{
-		sckHandle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	}
+	sckHandle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
 	if (sckHandle == INVALID_SOCKET)
 		throw std::exception("Faild to create handle");
+
+	memset(recvBuffer, 0, BUF_SIZE);
+	sendThread = std::thread{ sendThreadFunc, std::ref(*this) };
 }
 
 RUDPSocket::~RUDPSocket()
 {
+	loop = false;
+
+	if (sendThread.joinable())
+		sendThread.join();
+
 	if (sckHandle)
 		closesocket(sckHandle);
+}
+
+void RUDPSocket::connect()
+{
+	if (::connect(sckHandle, reinterpret_cast<sockaddr*>(&hostEp), sizeof(hostEp)) == SOCKET_ERROR)
+		throw std::exception("Failed to connect");
 }
 
 void RUDPSocket::bind(u_short port)
@@ -41,42 +50,108 @@ void RUDPSocket::bind(u_short port)
 	}
 }
 
+bool RUDPSocket::getFrontMsg(message& msg)
+{
+	std::unique_lock<std::mutex> lock{ mainQueMut };
+	if (mainSendQueue.empty()) return false;
+	
+	msg = mainSendQueue.front();
+	return true;
+}
+
 void RUDPSocket::printEndpoint()
 {
 	sockaddr_in sckAddr;
 	int len = sizeof(sckAddr);
 	getsockname(sckHandle, reinterpret_cast<sockaddr*>(&sckAddr), &len);
 
-	const int MAX_IP_LEN = 256;
-	char ip[MAX_IP_LEN];
-	inet_ntop(AF_INET, &sckAddr.sin_addr, ip, MAX_IP_LEN);
-
-	std::cout << "IP: " << ip << "\n";
-	std::cout << "Port: " << ntohs(sckAddr.sin_port) << "\n";
+	printIPAndPort(sckAddr);
 }
 
-void RUDPSocket::setHostEndpoint(const std::string& ip, const u_short port)
+void RUDPSocket::pushToSendBuffer(std::byte* packet, size_t pckSize, sockaddr_in& hostEp)
 {
-	hostEp.sin_family = AF_INET;
-	hostEp.sin_port = htons(port);
-	inet_pton(AF_INET, ip.c_str(), &hostEp.sin_addr);
+	message msg;
+	msg.peer = getPeerKey(hostEp);
+
+	msg.packet.resize(pckSize);
+	memcpy_s(msg.packet.data(), pckSize, packet, pckSize);
+
+	std::unique_lock<std::mutex> lock{ mainQueMut };
+	mainSendQueue.push(msg);
 }
 
-void RUDPSocket::sendTo(std::byte* packet, size_t pckSize)
+void RUDPSocket::sendTo(std::byte* packet, size_t pckSize, sockaddr_in& hostEp)
 {
 	if (sendto(sckHandle, reinterpret_cast<const char*>(packet), pckSize, 0,
 		reinterpret_cast<sockaddr*>(&hostEp), sizeof(hostEp)) == SOCKET_ERROR)
 	{
-		throw std::exception("Failed to send: " + WSAGetLastError());
+		throw std::exception("Failed to send to: " + WSAGetLastError());
 	}
 }
 
-void RUDPSocket::recvFrom(std::byte* packet, size_t bufSize)
+void RUDPSocket::recvFrom(std::byte* packet, size_t bufSize, sockaddr_in& senderEp)
 {
-	int fromLen = sizeof(hostEp);
+	int fromLen = sizeof(senderEp);
+
 	if (recvfrom(sckHandle, reinterpret_cast<char*>(packet), bufSize, 0,
-		reinterpret_cast<sockaddr*>(&hostEp), &fromLen) == SOCKET_ERROR)
+		reinterpret_cast<sockaddr*>(&senderEp), &fromLen) == SOCKET_ERROR)
 	{
-		throw std::exception("Failed to recv");
+		throw std::exception("Failed to recv from: " + WSAGetLastError());
+	}
+}
+
+void RUDPSocket::printIPAndPort(const sockaddr_in& addr)
+{
+	const int MAX_IP_LEN = 256;
+	char ip[MAX_IP_LEN];
+	inet_ntop(AF_INET, &addr.sin_addr, ip, MAX_IP_LEN);
+
+	std::cout << "IP: " << ip << "\n";
+	std::cout << "Port: " << ntohs(addr.sin_port) << "\n";
+}
+
+std::string RUDPSocket::getPeerKey(const sockaddr_in& addr)
+{
+	const int MAX_IP_LEN = 256;
+	char ip[MAX_IP_LEN];
+	inet_ntop(AF_INET, &addr.sin_addr, ip, MAX_IP_LEN);
+
+	std::string ipStr = ip;
+	u_short port = ntohs(addr.sin_port);
+
+	return ipStr + ':' + std::to_string(port);
+}
+
+sockaddr_in RUDPSocket::stringToEp(const std::string& peer)
+{
+	int split = 0;
+	for (int i = 0; i < peer.size(); i++)
+	{
+		if (peer[i] == ':')
+			split = i;
+	}
+
+	std::string ip = peer.substr(0, split);
+	u_short port = std::stoi(peer.substr(split+1));
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+
+	return addr;
+}
+
+void RUDPSocket::sendThreadFunc(RUDPSocket& sck)
+{
+	while (sck.loop)
+	{
+		message msg{};
+		while (sck.getFrontMsg(msg))
+		{
+			auto hostEp = stringToEp(msg.peer);
+			sck.sendTo(msg.packet.data(), msg.packet.size(), hostEp);
+		}
+		std::this_thread::sleep_for(10ms);
 	}
 }
--- RUDP/Network/RUDPSocket.h
+++ RUDP/Network/RUDPSocket.h
@@ -9,13 +9,24 @@
 
 #include <string>
 #include <cstddef>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <map>
+#include <mutex>
+
+struct message
+{
+	std::string peer;
+	std::vector<std::byte> packet;
+};
 
 class RUDPSocket
 {
 public:
 	enum class SckType
 	{
-		TCP,
 		UDP
 	};
 
@@ -23,14 +34,36 @@ public:
 	RUDPSocket(const SckType& type);
 	virtual ~RUDPSocket();
 
+	void connect();
 	void bind(u_short port);
+	
 	void printEndpoint();
 
-	void setHostEndpoint(const std::string& ip, const u_short port);
-	void sendTo(std::byte* packet, size_t pckSize);
-	void recvFrom(std::byte* buf, size_t bufSize);
+	void pushToSendBuffer(std::byte* packet, size_t pckSize, sockaddr_in& hostEp);
+
+	void sendTo(std::byte* packet, size_t pckSize, sockaddr_in& hostEp);
+	void recvFrom(std::byte* buf, size_t bufSize, sockaddr_in& senderEp);
 
 private:
+	bool getFrontMsg(message& msg);
+
+public:
+	static void printIPAndPort(const sockaddr_in& addr);
+	static std::string getPeerKey(const sockaddr_in& addr);
+	static sockaddr_in stringToEp(const std::string& peer);
+
+	static void sendThreadFunc(RUDPSocket& sck);
+
+private:
+	static const int BUF_SIZE = 256;
+	std::byte recvBuffer[BUF_SIZE];
+
 	SOCKET sckHandle;
 	sockaddr_in hostEp;
+
+	std::thread sendThread;
+	std::atomic_bool loop;
+
+	std::mutex mainQueMut;
+	std::queue<message> mainSendQueue;
 };
