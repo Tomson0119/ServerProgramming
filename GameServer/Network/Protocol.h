be9bdc161bd41272e9e235bfc95109c368d3d649
Tomson0119 <kjs980119@gmail.com>
2022-09-12 16:25:22 +0900

    NPC 개수 조정(테스트)

--- GameServer/Network/Protocol.h
+++ GameServer/Network/Protocol.h
@@ -7,7 +7,7 @@ const int  WORLD_WIDTH = 2000;
 const int  MAX_NAME_SIZE = 20;
 const int  MAX_CHAT_SIZE = 100;
 const int  MAX_USER = 10000;
-const int  MAX_NPC = 200000;
+const int  MAX_NPC = 50000;
 constexpr int NPC_ID_START = MAX_USER;
 constexpr int NPC_ID_END = MAX_USER + MAX_NPC - 1;
 
