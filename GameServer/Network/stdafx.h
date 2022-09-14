1efc5418e775a49bb633c1c0721927e7b210048f
Tomson0119 <kjs980119@gmail.com>
2022-09-14 21:05:25 +0900

    Player info의 변수들 atomic 화, 복사 생성자 및 대입 연산자 추가

--- GameServer/Network/stdafx.h
+++ GameServer/Network/stdafx.h
@@ -37,12 +37,49 @@ using namespace std::chrono_literals;
 struct PlayerInfo
 {
 	char name[MAX_NAME_SIZE];
-	short level;
-	short x;
-	short y;
-	short hp;
-	short max_hp;
-	int exp;
+	std::atomic<short> level;
+	std::atomic<short> x;
+	std::atomic<short> y;
+	std::atomic<short> hp;
+	std::atomic<short> max_hp;
+	std::atomic<int> exp;
+
+	PlayerInfo() : 
+		level{ -1 },
+		x{ -1 },
+		y{ -1 },
+		hp{ -1 },
+		max_hp{ -1 },
+		exp{ -1 }
+	{
+		memset(name, 0, MAX_NAME_SIZE);
+	}
+
+	PlayerInfo(const PlayerInfo& other) :
+		level{ other.level.load() },
+		x{ other.x.load() },
+		y{ other.y.load() },
+		hp{ other.hp.load() },
+		max_hp{ other.max_hp.load() },
+		exp{ other.exp.load() }
+	{
+		strncpy_s(name, other.name, MAX_NAME_SIZE);
+	}
+
+	PlayerInfo& operator=(const PlayerInfo& other)
+	{
+		if (this != &other)
+		{
+			level = other.level.load();
+			x = other.x.load();
+			y = other.y.load();
+			hp = other.hp.load();
+			max_hp = other.max_hp.load();
+			exp = other.exp.load();
+			strncpy_s(name, other.name, MAX_NAME_SIZE);
+		}
+		return *this;
+	}
 };
 
 inline char GetPacketType(std::byte* pck)
